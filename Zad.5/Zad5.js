b0b4206da1aa261bbfe80d3fab874f54b77fa02a
ddr997 <73550027+ddr997@users.noreply.github.com>
2021-04-12 10:47:30 +0200

    Update Zad5.js

--- Zad.5/Zad5.js
+++ Zad.5/Zad5.js
@@ -27,3 +27,25 @@ request.onupgradeneeded = function (event) {
         objectStore.add(employeeData[i]);
     }
 }
+
+function loadTable() {
+    var employees = "";
+    $('.employee').remove();
+
+    var objectStore = db.transaction("employee").objectStore("employee");
+    objectStore.openCursor().onsuccess = function (event) {
+        var cursor = event.target.result;
+        if (cursor) {
+            employees = employees.concat(
+                '<tr class="employee">' +
+                '<td class="ID">' + cursor.key + '</td>' +
+                '<td class="Imie">' + cursor.value.name + '</td>' +
+                '<td class="Wiek">' + cursor.value.age + '</td>' +
+                '<td class="Email">' + cursor.value.email + '</td>' +
+                '</tr>');
+            cursor.continue(); // wait for next event
+        } else {
+            $('thead').after(employees); // no more events
+        }
+    };
+}

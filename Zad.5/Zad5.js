d536bad49e859dcbf4bc39d6e6f4ad31bd23943e
ddr997 <73550027+ddr997@users.noreply.github.com>
2021-04-12 11:32:32 +0200

    .

--- Zad.5/Zad5.js
+++ Zad.5/Zad5.js
@@ -2,6 +2,7 @@ window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndex
 window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
 window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
 
+
 if (!window.indexedDB) {
     window.alert("Brak wsparcia IndexedDB na twoja przegladarke.")
 }
